3c2d9abedd23c197cee9bf6fd48a0ae28758c357
GursimranSinghKahlon <gursimransingh19970@gmail.com>
2018-08-05 11:55:03 +0530

    Updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python competo.py
--- competo.py
+++ competo.py
@@ -1,12 +1,14 @@
 import requests
-import urllib2 as urllib  
+#import urllib3 as urllib  
 from bs4 import BeautifulSoup
-from pprint import pprint
+#from pprint import pprint
 import re
+import datetime
+from time import sleep
 
-TOKEN = "#########################"
+TOKEN = "#####################"
 URL = "https://www.techgig.com/challenge"
-CHAT_ID = "########"
+CHAT_ID = "############"
 
 class BotHandler:
 
@@ -20,16 +22,59 @@ class BotHandler:
         resp = requests.post(self.api_url + method, params)
         return resp
 
-bot = BotHandler(TOKEN)
 
-html_doc=urllib.urlopen(URL).read()
-soup = BeautifulSoup(html_doc, 'lxml')
-listit = soup.select('div#live-contest-listing div header a')
+def fetch(details):
+    #html_doc=urllib.urlopen(URL).read()
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content,"html.parser")
+    listit = soup.select('div#live-contest-listing div header a')
 
-details = []
-for competition in listit:
-    details.append(re.sub('[\\n]+',' ',competition.attrs['href'] + ' ' +competition.text))
+    for competition in listit:
+        details.append(re.sub('[\\n]+',' ',competition.attrs['href'] + ' ' +competition.text))
 
-pprint(details)
-for item in details:
-    print(bot.send_message(item,CHAT_ID))
+
+def main():
+    bot = BotHandler(TOKEN)
+    bot.send_message('Deployed successfully. ',CHAT_ID)
+    while(True):
+        now = datetime.datetime.now()    
+        #print(now.hour)   
+        #bot.send_message(now.hour,CHAT_ID)     
+        if now.hour==0:
+            break  
+        sleep(30)
+
+    while(True):
+        details = []
+        details2 = []
+        fetch(details)
+        for item in details:
+            #print(item)
+            bot.send_message(item,CHAT_ID)
+        t=0
+
+        #Check for any new competition every 24 hour
+        while(True):
+            sleep(3600 * 24)
+            #sleep(10)   
+             
+            #Reset every third day : Receive list of all competitions         
+            t+=1
+            if t == 3:
+                break   
+
+            fetch(details2)
+            #pprint(details2)
+            bot.send_message('New item : ',CHAT_ID)
+            for item in details2:
+                if item  not in details:
+                    #print(item)
+                    #print('New competition : ')
+                    bot.send_message('New competition : ',CHAT_ID)
+                    bot.send_message(item,CHAT_ID)
+
+if __name__ == '__main__':  
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit()
